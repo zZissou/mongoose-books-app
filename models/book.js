95e6cdb310d7d812638bbc63c16bb750e9960ef5
zZissou <kyle.edward.russell@gmail.com>
2016-11-10 11:05:49 -0800

    made some changes to some stuff.

--- models/book.js
+++ models/book.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-  title: String,
-  author: String,
-  image: String,
-  releaseDate: String
+    title: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'Author',
+    },
+    image: String,
+    releaseDate: String
 });
 
 var Book = mongoose.model('Book', BookSchema);
--- server.js
+++ server.js
@@ -23,10 +23,37 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) { return console.log("index error: " + err); }
-    res.json(books);
+  db.Book.find()
+    // populate fills in the author id with all the author data
+    .populate('author')
+    .exec(function(err, books){
+      if (err) { return console.log("index error: " + err); }
+      res.json(books);
+    });
+});
+
+// create new book
+app.post('/api/books', function (req, res) {
+  // create new book with form data (`req.body`)
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
   });
+
+  // this code will only add an author to a book if the author already exists
+  db.Author.findOne({name: req.body.author}, function(err, author){
+    newBook.author = author;
+    // add newBook to database
+    newBook.save(function(err, book){
+      if (err) {
+        return console.log("create error: " + err);
+      }
+      console.log("created ", book.title);
+      res.json(book);
+    });
+  });
+
 });
 
 // get one book
